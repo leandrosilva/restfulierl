dc8316e4f3334fbcaf5939d56141b4ef826904f2
Leandro Silva <leandrodoze@gmail.com>
2010-01-01 05:15:27 -0800

    refactor

--- src/restfulierl_xml_parser.erl
+++ src/restfulierl_xml_parser.erl
@@ -49,7 +49,9 @@ parse_xml_element(Xml) ->
 	
 	case Name of
 		'atom:link' ->
-			_ParsedTransition = parse_xml_atom_link_attributes(Attributes);
+			{Rel, Href} = parse_xml_atom_link_attributes(Attributes),
+			
+			_ParsedTransition = #transition{name = Rel, uri = Href};
 		_ ->
 			_ParsedElement = {Name, parse_xml_attributes(Attributes), parse_xml_children(Content)}
 	end.
@@ -66,6 +68,15 @@ parse_xml_attributes([HeadAttribute | TailAttributes], ParsedAttributes) ->
 parse_xml_attributes([], ParsedAttributes) ->
 	lists:reverse(ParsedAttributes).
 	
+%% parse the atom:link as transition
+parse_xml_atom_link_attributes(Attributes) ->
+	ParsedAttributes = parse_xml_attributes(Attributes),
+
+	[{attribute, rel, Rel} | NextAttributes] = ParsedAttributes,
+	[{attribute, href, Href} | _] = NextAttributes,
+
+	{Rel, Href}.
+		
 %% parse the element's childen
 parse_xml_children(Elements) ->
 	parse_xml_children(Elements, []).
@@ -73,6 +84,8 @@ parse_xml_children(Elements) ->
 parse_xml_children([{xmlText, _, _, _, Value, text} | TailElements], ParsedElements) ->
 	case TailElements of
 		[{xmlElement, _, _, _, _, _, _, _, _, _, _, _} | _] ->
+			parse_xml_children(TailElements, ParsedElements);
+		[] ->
 			parse_xml_children(TailElements, ParsedElements);
 		_ ->
 			parse_xml_children(TailElements, [Value | ParsedElements])
@@ -84,12 +97,3 @@ parse_xml_children([HeadElement | TailElements], ParsedElements) ->
 
 parse_xml_children([], ParsedElements) ->
 	lists:reverse(ParsedElements).
-
-%% parse the atom:link as transition
-parse_xml_atom_link_attributes(Attributes) ->
-	ParsedAttributes = parse_xml_attributes(Attributes),
-	
-	[{attribute, rel, Rel} | NextAttributes] = ParsedAttributes,
-	[{attribute, href, Href} | _] = NextAttributes,
-	
-	_ParsedTransition = #transition{name = Rel, uri = Href}.
